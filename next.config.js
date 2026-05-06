50d673abf980d33254da898a8b98aa18a51ab32e
Ishan Kanani <ishankanani@gmail.com>
2026-05-06 22:20:06 +0200

    upgrade nextjs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@prisma/engines/**',
+      'node_modules/@prisma/client/**',
+      'node_modules/.prisma/**',
+      'node_modules/prisma/**',
+    ],
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'img.youtube.com' },
